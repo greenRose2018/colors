e84eb58dbf500c7a24d2b22c0b17b559fa38170e
Jordan Hudgens <jordan@devcamp.com>
2017-06-21 17:09:09 +0000

    Implemented customscopes for portfolio items

--- DevcampPortfolio/config/routes.rb
+++ DevcampPortfolio/config/routes.rb
@@ -1,6 +1,7 @@
 Rails.application.routes.draw do
   resources :portfolios, except: [:show]
-  
+  get 'angular-items', to: 'portfolios#angular'
+  get 'ruby-on-rails', to: 'portfolios#rails'
   get 'portfolio/:id', to: 'portfolios#show', as: 'portfolio_show'
   
   get 'about', to: 'pages#about'
